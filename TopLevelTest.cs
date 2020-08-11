b094758fb2a2e7fc1a60af6ea4ce866af28f1418
William Menegaldi <44971939+WillMenegaldi@users.noreply.github.com>
2020-08-11 01:06:56 -0300

    first commit
    creating the project

---
+++ TopLevelTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.Write("Entre com um número: ");
+
+var valor = new Random().Next(-5, 16);
+
+var mensagem = valor switch
+{
+    <= 0 => "Menor ou igual a 0!",
+    > 0 and <= 10 => "Entre 0 e 10!",
+    _ => "Maior do que 10!"
+};
+
+Console.WriteLine($"Número: {valor}\nMensagem: {mensagem}");
